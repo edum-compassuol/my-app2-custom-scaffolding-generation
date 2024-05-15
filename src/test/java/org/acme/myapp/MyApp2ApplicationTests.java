46610ae8a3330a9d1d06dd8da3dde7c215ea4ccb
eduardomallmann <mallmann.edu@gmail.com>
2024-05-15 11:13:49 -0300

    Add initial scaffolding structure

---
+++ src/test/java/org/acme/myapp/MyApp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.acme.myapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyApp2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
